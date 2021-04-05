e724d6fcf44a9093740211abf94dbc95a664ee42
gillout <54893913+gillout@users.noreply.github.com>
2021-04-05 22:08:16 +0200

    Refactor : Modification du template, avec ajout de la possibilité de … (#26)
    * Refactor : Modification du template, avec ajout de la possibilité de modifier lors de la validation (ajout, modification)
    * Correction PR

--- src/app/back/view/validation.php
+++ src/app/back/view/validation.php
@@ -3,22 +3,14 @@ use App\Back\Util\Form;
 
 if (isset($form) && $form instanceof Form) {
 ?>
+
     <section id="sect-valid">
 
         <h1>Demande de Confirmation</h1>
 
-        <form method="POST">
-
-            <?php foreach($form->getDatas() as $name => $value) :
-                echo $form->input($name, null, ['type' => 'hidden', 'value' => $value]);
-            endforeach; ?>
-
-            <p>Etes-vous sûr ?</p>
-
-            <button class="btn btn-success" name="validate" value="true">Confirmer</button>
-            <p><a class="btn btn-secondary" href="?target=all">Annuler</a></p>
-
-        </form>
+        <?php
+        require_once $_GET['target'] == 'delete' ? ROOT_DIR . 'back/view/fragment/toValidateDelete.php' : ROOT_DIR . 'back/view/fragment/toValidateAddOrModify.php';
+        ?>
 
     </section>
 <?php
